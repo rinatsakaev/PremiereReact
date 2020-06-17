77ae2fcb5af21f2cfff34f1d30b1e39babbada21
rinatsakaev <r.sakaevv@gmail.com>
2020-06-18 00:07:44 +0500

    styles2

--- PremiereReact/ClientApp/src/components/session/session.js
+++ PremiereReact/ClientApp/src/components/session/session.js
@@ -4,9 +4,11 @@ export default function Session({startTime, onDelete}) {
     const date = new Date(startTime);
     const days = date.getDate().toString().padStart(2, "0");
     const months = (date.getMonth()+1).toString().padStart(2, "0");
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
     return (
         <li className={"session__item"}>
-            <p className={"session__start-time"}>{`${days}.${months} ${date.getHours()}:${date.getMinutes()}`}</p>
+            <p className={"session__start-time"}>{`${days}.${months} ${hours}:${minutes}`}</p>
             <button className={"session__remove"} onClick={() => onDelete()}>Удалить</button>
         </li>
     )
